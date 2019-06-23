4fd6ad93818b423b0095a8333d8a98a62a766d0f
Daryl Cecile <6310278+daryl-cecile@users.noreply.github.com>
2019-06-23 16:14:56 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,11 @@
 
 // More Information about this assisngment is available in the README.md file
 
+let answerArr = [];
+let oldConsoleLog = console.log;
+console.log = (...args)=>{
+	oldConsoleLog.call(this,...args);
+	answerArr.push(args);
+};
+
+// WRITE YOUR PROGRAM BELOW
