49c6a395b6b378d0f4c777bdc898bcac43c120d0
Daryl <darylcecile@gmail.com>
2022-05-09 23:40:17 +0100

    updated tests

--- _campus/test.js
+++ _campus/test.js
@@ -1,19 +1,19 @@
 describe('Output should contain 100 elements', ()=>{
-	expect(answerArr.length).toEqual(100);
+	expect(answerArr.length).to.equal(100);
 });
 
 describe('The 6th item should be Fizz', ()=>{
-	expect(answerArr[5]).toEqual('Fizz');
+	expect(answerArr[5]).to.equal('Fizz');
 });
 
 describe('The 50th item should be Buzz', ()=>{
-	expect(answerArr[49]).toEqual('Buzz');
+	expect(answerArr[49]).to.equal('Buzz');
 });
 
 describe('The 90th item should be FizzBuzz', ()=>{
-	expect(answerArr[89]).toEqual('FizzBuzz');
+	expect(answerArr[89]).to.equal('FizzBuzz');
 });
 
 describe('The 98th item should be 98', ()=>{
-	expect(answerArr[97]).toBeVaguelyEqual('98');
+	expect(answerArr[97]).to.be.oneOf(['98', 98]);
 });
